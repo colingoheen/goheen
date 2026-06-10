b23ffc9929b17f3b730f5c758bfa065e7f67672f
Colin Goheen <colingoheen@Mac.attlocal.net>
2026-06-09 17:36:38 -0700

    update title

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image"
 import { WebGLShader } from "@/components/ui/web-gl-shader"
-import { LiquidButton } from "@/components/ui/liquid-glass-button"
 
 export default function Home() {
   return (
@@ -10,18 +9,6 @@ export default function Home() {
         <main className="relative py-10 overflow-hidden">
           <div className="mb-3 flex justify-center">
             <Image src="/goheen_logo_white.png" alt="Goheen" width={800} height={240} priority />
-          </div>
-          <div className="my-8 flex items-center justify-center gap-1">
-            <span className="relative flex h-3 w-3 items-center justify-center">
-              <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-500 opacity-75"></span>
-              <span className="relative inline-flex h-2 w-2 rounded-full bg-green-500"></span>
-            </span>
-            <p className="text-xs text-green-500">Available for New Projects</p>
-          </div>
-          <div className="flex justify-center">
-            <LiquidButton className="text-white border rounded-full" size="xl">
-              Let's Go
-            </LiquidButton>
           </div>
         </main>
       </div>
